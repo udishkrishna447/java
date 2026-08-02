0e36c491951573deaf6f22c12efd0d74bb087f66
udishkrishna447 <udishkrishna447@gmail.com>
2026-08-02 10:45:53 +0530

    Time: 5 ms (61.65%), Space: 45.9 MB (51.22%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,23 +3,10 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        int freq1[]= new int[26];
-        int freq2[]=new int [26];
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            int value=ch-'a';
-            freq1[value]++;
-        }
-        for(int i=0;i<t.length();i++){
-            char ch=t.charAt(i);
-            int value=ch-'a';
-            freq2[value]++;
-        }
-        for(int i=0;i<26;i++){
-            if(freq1[i]!=freq2[i]){
-                return false;
-            }
-        }
-        return true;
+        char[] first = s.toCharArray();
+        char[] second = t.toCharArray();
+        Arrays.sort(first);
+        Arrays.sort(second);
+        return Arrays.equals(first,second);
     }
 }
