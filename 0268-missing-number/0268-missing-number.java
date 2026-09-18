fbce3471f911d347ffc48d7ff11646c27d1b4291
udishkrishna447 <udishkrishna447@gmail.com>
2026-09-18 17:16:28 +0530

    Time: 8 ms (16.25%), Space: 48.3 MB (5.47%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,14 +1,14 @@
+import java.util.Arrays;
+
 class Solution {
     public int missingNumber(int[] nums) {
-         HashSet<Integer> set = new HashSet<>();
-         for(int num:nums){
-            set.add(num);
-         }
-         for(int i=0;i<=nums.length;i++){
-            if(!set.contains(i)){
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i) {
                 return i;
             }
-         }
-         return -1;
+        }
+        return nums.length;
     }
 }
