80a2dd19afb33b2cafef9916e30eb8ec30778f56
udishkrishna447 <udishkrishna447@gmail.com>
2026-09-19 17:00:45 +0530

    Time: 4 ms (83.69%), Space: 44.1 MB (89.32%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,18 +1,24 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int freq1[] = new int[26];
-        int freq2[] = new int[26];
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int freq1[]=new int[26];
+        int freq2[]=new int[26];
         for(int i=0;i<s.length();i++){
-            int value1 = s.charAt(i)-'a';
-            freq1[value1]++;
+            char temp=s.charAt(i);
+            int value=temp-'a';
+            freq1[value]++;
         }
         for(int i=0;i<t.length();i++){
-            int value1 = t.charAt(i)-'a';
-            freq2[value1]++;
+            char temp=t.charAt(i);
+            int value=temp-'a';
+            freq2[value]++;
         }
-        for(int i=0;i<26;i++)
-        if(freq1[i]!=freq2[i]){
-            return false;
+        for(int i=0;i<26;i++){
+            if(freq1[i]!=freq2[i]){
+                return false;
+            }
         }
         return true;
     }
